c7df2e946f2af2c05dd2db8180e6faa8bb075767
hk0916 <mhg5303@gmail.com>
2019-09-07 00:46:39 +0900

    speed up input output

---
+++ common/input_output.py
@@ -0,0 +1,16 @@
+import sys
+'''
+3
+1
+1
+1
+'''
+n = int(sys.stdin.readline())
+a = [sys.stdin.readline() for i in range(n)]
+list((map(int, a)))
+# 3 7
+a, b = map(int, sys.stdin.readline().strip().split())
+# 3 7 8
+list(map(int, sys.stdin.readline().strip().split()))
+i = 7
+sys.stdout.write(str(i)+'\n')
