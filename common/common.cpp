87b47f1e6fd62306c37903722ec5c680b63d9926
HyunKyun Moon <mhg5303@gmail.com>
2020-05-07 10:29:12 +0900

    update common

--- common/common.cpp
+++ common/common.cpp
@@ -1,4 +1,5 @@
 #pragma warning(disable:4996)
+#include <bits/stdc++.h>
 #include <algorithm>
 #include <regex>
 #include <string>
--- common/input_output.cpp
+++ common/input_output.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false); // no with getchar
+    // ios::sync_with_stdio(false); // no with getchar
     // cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // freopen("input.txt", "r", stdin);
     return 0;
 }
