117e5e7d9ecec7a6f3c121bb622937a166b63730
HyunKyun Moon <mhg5303@gmail.com>
2020-10-15 14:58:22 +0900

    15552

---
+++ acmicpc/cpp/15552.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+
+	int t, a, b;
+	cin >> t;
+	while (t--) {
+		cin >> a >> b;
+		cout << a+b << '\n';
+	}
+
+	return 0;
+}
